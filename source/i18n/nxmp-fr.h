49d6ed70c5c32819ce4a2c062da5f0d342ab9fe1
Zoria <50277488+THZoria@users.noreply.github.com>
2023-10-03 18:57:10 +0200

    fix French translation

--- source/i18n/nxmp-fr.h
+++ source/i18n/nxmp-fr.h
@@ -1,13 +1,13 @@
 #ifndef NXMP_I18N_FR_H
-#daffiner NXMP_I18N_FR_H
+#define NXMP_I18N_FR_H
 
-/ * Langue par défaut * /
-const char * fr_i18n_author = "proconsule";
-bool fr_i18n_status = faux ;
-bool fr_i18n_native = vrai ;
+/* Default Language */
+const char *fr_i18n_author = "proconsule";
+bool fr_i18n_status = false;
+bool fr_i18n_native = true;
 
-const char * MainMenu_STR_FR[] = {
-	"Fichier locaux",
+const char *MainMenu_STR_FR[] = {
+	"Fichiers locaux",
 	"USB",
 	"Flux URL",
 	"Réseau",
@@ -16,67 +16,67 @@ const char * MainMenu_STR_FR[] = {
 	"Liste de lecture",
 	"Paramètres",
 	"Info",
-	"Sortie"
+	"Quitter"
 };
 
-const char * Common_STR_FR[] = {
+const char *Common_STR_FR[] = {
 	"Oui",
 	"Non",
-	"D'ACCORD",
+	"OK",
 	"Auto",
 	"Navigation",
 	"Sélection",
-	"Sélection / Connexion",
+	"Sélection/Lecture",
 	"Accueil",
-	"Accueil / Sauvegarde",
+	"Accueil/Sauvegarde",
 	"Retour",
 	"Menu",
 	"Nom",
 	"Taille",
 	"Date",
-	"Monté",
-	"Diminué",
-	"Coupleur",
-	"Quitté",
-	"Annulé",
-	"Sauvegardé",
+	"Monter",
+	"Descendre",
+	"Supprimer",
+	"Quitter",
+	"Annuler",
+	"Sauvegarder",
 };
 
 
-const char * Popup_STR_FR[] = {
+const char *Popup_STR_FR[] = {
 	"Sauvegarde des paramètres",
 	"Reprise de la lecture",
 	"Mise à jour de la base de données",
-	"La base de données à été mis à jour",
+	"La base de données à été mise à jour",
 	"Menu réseau",
 	"Menu des fichiers",
-	"Tri croissant (nom)",
-	"Tri croissant (taille)",
-	"Tri croissant (date)",
+	"Tri croissant (Nom)",
+	"Tri croissant (Taille)",
+	"Tri croissant (Date)",
 	"Tri en ordre décroissant (Nom)",
-	"Tri en ordre décroissant (taille)",
-	"Tri descendant (date)",
+	"Tri en ordre décroissant (Taille)",
+	"Tri descendant (Date)",
 	"Ajouter un fichier à la liste de lecture",
 	"Ajouter Vérifié à la liste de lecture",
 	"Supprimer élément",
 	"Supprimer la liste de lecture",
-	"Défini comme élément de départ"
+	"Définir comme élément de départ"
 };
 
-const char * SettingsMenu_STR_FR[] = {
-	"Généraux",
+const char *SettingsMenu_STR_FR[] = {
+	"Généralités",
 	"Parcourir les fichiers",
-	"Afficher les fiches cachés",
+	"Afficher les fichiers cachés",
 	"Navigation",
-	"Active les commandes tactiles",
+	"Activer les commandes tactiles",
 	"Overclock",
-	"Acteur l'horloge au débarquement",
-	"Divers (redémarrage nécessaire)",
+	"Activer l'overclock au démarrage",
+	"Divers (Nécessite de redémarrer)",
 	"VSYNC",
 	"Langue de l'interface",
 	"Paramètres du lecteur",
 	"Décodeur",
-	"HW Decodeur",
+	"Décodeur HW",
 	"Demux Cache",
 	"Demux Cache Sec",
 	"Audio",
@@ -90,38 +90,38 @@ const char * SettingsMenu_STR_FR[] = {
 	"Utiliser la langue des sous-titres automatiques",
 	"Langue des sous-titres",
 	"Sous-titres",
-	"Taille de la sous-police",
-	"Échelle des sous-polices",
+	"Taille des sous-titres",
+	"Échelle des sous-titres",
 	"Vidéo",
-	"Déhentrelacé vidéo",
+	"Désentrelacement vidéo",
 	"Base de données",
 	"Utiliser la base de données",
-	"Version de la base de données:",
-	"Version de SQLite:",
-	"Reprise du compte des points:",
-	"Vue complétée:",
+	"Version de la base de données :",
+	"Version de SQLite :",
+	"Reprise du décompte des points : ",
+	"Vue complétée : ",
 	"La base de données n'est pas active",
-	"Commencer à enregistrer les informations:",
-	"Cesser d'enregistrer les informations:",
+	"Commencer à enregistrer les informations :",
+	"Cesser d'enregistrer les informations :",
 	
 	"Paramètres tactiles",
 };
 
-const char * Network_STR_FR[] = {
+const char *Network_STR_FR[] = {
 	"Ajout d'un partage",
 	"Ajout d'un partage au menu",
 	"Configuration du partage",
-	"Nom du partage:",
-	"Protocole:",
-	"Identifications",
-	"Adresse IP:",
-	"Port:",
-	"Identifiant:",
-	"Mot de passe:",
+	"Nom du partage :",
+	"Protocole :",
+	"Références",
+	"Adresse IPs :",
+	"Port :",
+	"Identifiant :",
+	"Mot de passe :",
 	"Anonyme",
 	"Chemin d'accès à distance:",
-	"Chemin d'accès distant sur le serveur exemple: / film /",
-	"Partage à distance sur le serveur (le partage n'est pas le chemin))",
+	"Chemin d'accès distant sur le serveur exemple : /movie/",
+	"Partage à distance sur le serveur (ne partage pas le chemin))",
 	"Test de connexion",
 	"Connexion OK",
 	"Erreur de connexion",
